3c07ceb9ab95b0b3b5154db74f6c3a18cf6ad02c
Rafal Podlipny <rafal0p@outlook.com>
2018-01-21 17:34:00 +0100

    download page content

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var http = require('http');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({'respond': ' with json'});
+var getPageContent = function (succ) {
+    http.get(
+        'http://jakoszczedzacpieniadze.pl',
+        (getRes) => {
+            getRes.setEncoding('utf8');
+            let fullPage = '';
+            getRes.on('data', (chunk) => {
+                fullPage += chunk;
+            })
+            getRes.on('end', () => {
+                succ(fullPage);
+            })
+        }
+    );
+}
+
+router.get('/', function (req, res, next) {
+    getPageContent(content => {
+        res.json({ 'respond': content });
+    });
 });
 
 module.exports = router;
