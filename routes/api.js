1e2106b42037b979ecdf5b81a1c48604c30aad57
Rafal Podlipny <rafal0p@outlook.com>
2018-01-21 18:57:01 +0100

    select only podcast content

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,26 @@
 var express = require('express');
 var request = require('request');
+var htmlparser = require('htmlparser');
+var select = require('soupselect').select;
+var html = require('htmlparser-to-html');
 var router = express.Router();
 
 var getPodcast = (number, cb) => {
     request(
         'http://jakoszczedzacpieniadze.pl/' + number,
         (err, res, body) => {
-            cb(body);
+            var handler = new htmlparser.DefaultHandler();
+            var parser = new htmlparser.Parser(handler);
+            parser.parseComplete(body);
+            var pressrelease = select(handler.dom, 'div.pressrelease-content');
+            cb(html(pressrelease));
         }
     )
 };
 
 router.get('/', function (req, res, next) {
     getPodcast('099', (body) => {
-        res.json({ 'respondx': body });
+        res.json({ 'response': body });
     });
 });
 
