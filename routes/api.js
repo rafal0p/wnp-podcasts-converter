4e4da4574e558c0a1eb093f28a17f930f5f088ec
Rafal Podlipny <rafal0p@outlook.com>
2018-01-21 17:49:51 +0100

    rewrite get with `request` library

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,14 @@
 var express = require('express');
-var http = require('http');
+var request = require('request');
 var router = express.Router();
 
-var getPageContent = function (succ) {
-    http.get(
-        'http://jakoszczedzacpieniadze.pl',
-        (getRes) => {
-            getRes.setEncoding('utf8');
-            let fullPage = '';
-            getRes.on('data', (chunk) => {
-                fullPage += chunk;
-            })
-            getRes.on('end', () => {
-                succ(fullPage);
-            })
-        }
-    );
-}
-
 router.get('/', function (req, res, next) {
-    getPageContent(content => {
-        res.json({ 'respond': content });
-    });
+    request(
+        'http://jakoszczedzacpieniadze.pl',
+        (err, _, body) => {
+            res.json({ 'respond': body });
+        }
+    )
 });
 
 module.exports = router;
