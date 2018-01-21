22a9b9e385527530983e5c81204149f30b25700f
Rafal Podlipny <rafal0p@outlook.com>
2018-01-21 18:36:04 +0100

    create function getPodcast

--- routes/api.js
+++ routes/api.js
@@ -2,13 +2,19 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
+var getPodcast = (number, cb) => {
     request(
-        'http://jakoszczedzacpieniadze.pl',
-        (err, _, body) => {
-            res.json({ 'respond': body });
+        'http://jakoszczedzacpieniadze.pl/' + number,
+        (err, res, body) => {
+            cb(body);
         }
     )
+};
+
+router.get('/', function (req, res, next) {
+    getPodcast('099', (body) => {
+        res.json({ 'respondx': body });
+    });
 });
 
 module.exports = router;
