b4258441e4c00725cca604ae72f078284a3dda4d
Rafal Podlipny <rafal0p@outlook.com>
2018-01-23 20:46:10 +0100

    index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Więcej niż oszczędzanie pieniędzy.' });
 });
 
 module.exports = router;
